0de7d6096e4fac8af842de43efb215539c180b3f
Aria <applied7076@naver.com>
2020-07-01 15:18:58 +0900

    token verification

--- 2020 Node.js/60_mongodb/api/user/user.ctrl.js
+++ 2020 Node.js/60_mongodb/api/user/user.ctrl.js
@@ -1,5 +1,6 @@
 const UserModel = require("../../models/user");
 const bcrypt = require("bcrypt"); 
+const jwt = require("jsonwebtoken");
 
 const showSignupPage = (req, res) => {
     res.render("user/signup");
@@ -46,9 +47,49 @@ const login = (req, res) => {
             if (!isMatch) return res.status(500).send("비밀번호 틀림");
 
             // 다 맞으면 토큰 발급 (jsonwebtoken)
-            
+            const token = jwt.sign(user._id.toHexString(), "secret");
+            UserModel.findByIdAndUpdate(user._id, { token }, (err, result) => {
+                if (err) return res.status(500).send("로그인 시 오류..");
+                // 토큰 저장
+                res.cookie("token", token, { httpOnly: true });
+                res.json(result);
+            });
         });
     });
-}
+};
 
-module.exports = { showSignupPage, showLoginPage, signup, login };
+// verification
+const checkAuth = (req, res, next) => {
+    // 모든 화면 공통 사용 data
+    res.locals.user = null;
+    
+    // 쿠키에서 토큰 가져옴
+    const token = req.cookies.token;
+
+    if (!token) {
+        // 정상적: 로그인 안 한 경우
+        if (req.url === "/" || 
+            req.url === "/api/user/signup" ||
+            req.url === "/api/user/login")
+            return next();
+
+        // 비정상적
+        else return res.render("user/login");
+    }
+    // token verify
+    jwt.verify(token, "secret", (err, _id) => {
+        if (err) {
+            res.clearCookie("token");
+            return res.render("user/login");
+        }
+        
+        UserModel.findOne({ _id, token }, (err, user) => {
+            if (err) return res.status(500).send("인증 오류");
+            if (!user) return res.render("user/login");
+            res.locals.user = { name: user.name, role: user.role };
+            next();
+        });
+    });
+};
+
+module.exports = { showSignupPage, showLoginPage, signup, login, checkAuth };
